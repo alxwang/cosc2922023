630e95c82b801fe974f9546e41147d1dee139f21
Alex Wang <wangxi@saskpolytech.ca>
2023-01-20 15:03:08 -0600

    io

--- code/fileio/App.c
+++ code/fileio/App.c
@@ -3,5 +3,16 @@
 //
 #include "fileio.h"
 int main(int argc, const char * argv[]) {
-    demoOpenClose("CMakeCache.txt","r");
+    //demoOpenClose("CMakeCache.txt","r");
+    //demoReadTextFile("..\\fileio\\foo.txt","r");
+    //demofgetc("..\\fileio\\foo.txt","r");
+    //demofgets("..\\fileio\\foo.txt","r");
+    //demoWriteTextPrintf("..\\fileio\\foo1.txt","w","Alex");
+    //demoWriteTextfputs("..\\fileio\\foo1.txt","w","Sask Polytech");
+    //demoWriteTextfputc("..\\fileio\\foo1.txt","w","CST");
+    //execisefputs("..\\fileio\\foo1.txt","w","alex");
+//    demoBinWrite("..\\fileio\\goo.bin");
+//    demoBinRead("..\\fileio\\goo.bin");
+    ExerciseBinWrite("..\\fileio\\foo.bin");
+    ExerciseBinWrite("..\\fileio\\foo.bin");
 }
